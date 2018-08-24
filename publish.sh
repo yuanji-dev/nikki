e68fac2e6c69056b8f0d49f4f4889898fa3ed2ae
Gimo <self@gimo.me>
2018-08-24 15:53:16 +0900

    update publish script

--- publish.sh
+++ publish.sh
@@ -22,4 +22,4 @@ echo "Generating site"
 hugo
 
 echo "Updating gh-pages branch"
-cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
+cd public && git add --all && git commit -m "Publishing to gh-pages"
